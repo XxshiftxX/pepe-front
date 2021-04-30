4e19a6422cee5bb4784d9feff71f471301383407
XxshiftxX <contact@dev-shift.me>
2021-04-30 20:54:31 +0900

    fix: add root on index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import Root from './root';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Root />
   </React.StrictMode>,
   document.getElementById('root')
 );
